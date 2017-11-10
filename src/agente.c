e23325405bf12dca2f5c1fabe8b435c60d5f9e95
Paulo Tobias <paulohtobias@outlook.com>
2017-11-10 19:20:59 -0200

    Mudança de X e Y pra linha e coluna
    As correções nas chamadas de funções foram corrigidas

--- src/agente.c
+++ src/agente.c
@@ -3,122 +3,125 @@
 char salva_retorno[16] = {0};
 
 void __preenche_mundo_conhecido(int valor){
-	int i,j;
-	for(i=0; i < TAM_MAPA; i++){
+	int i, j;
+	for(i = 0; i < TAM_MAPA; i++){
 		for(j = 0; j < TAM_MAPA; j++){
 			player.mundo_conhecido[i][j] = valor;
 		}
 	}
-	player.mundo_conhecido[3][0] = mapa[3][0];
+	player.mundo_conhecido[TAM_MAPA - 1][0] = mapa[TAM_MAPA - 1][0];
 }
 
 void inicializa_jogador(){
 	__preenche_mundo_conhecido(0);
 	player.direcao = NORTE;
-	player.mundo_conhecido[3][0] = JOGADOR;
-	player.y = 3; player.x = 0;
-	mapa[3][0] = JOGADOR;
-    player.pontos = 0; // definindo pontuação inicial
-}					   // para teste.
+	player.mundo_conhecido[TAM_MAPA - 1][0] = JOGADOR;
+	player.linha = TAM_MAPA - 1;
+	player.coluna = 0;
+	mapa[TAM_MAPA-1][0] = JOGADOR;
+	player.pontos = 0; // definindo pontuação inicial.
+}
 
 void pontuar(int pontos){
-        
-    player.pontos += pontos;    
+
+	player.pontos += pontos;
 }
 
 void andar(SENTIDO sentido){
-        
-	rotacionar(sentido);	
-    switch(player.direcao){
-	
+
+	rotacionar(sentido);
+	switch(player.direcao){
+
 		case NORTE:
-			player.y--;
+			player.linha--;
 			break;
 		case LESTE:
-			player.x++;
+			player.coluna++;
 			break;
 		case SUL:
-			player.y++;
+			player.linha++;
 			break;
 		case OESTE:
-			player.x--;
+			player.coluna--;
 			break;
 	}
-	pontuar(-1);    
+	pontuar(-1);
 }
 
 void rotacionar(SENTIDO newSentido){
-    
+
 	if(newSentido != player.direcao){
-			    
+
 		if(MODULO(player.direcao - newSentido) == 2){
 			// Se for preciso movimento de 180° 
 			// penalizr o agente em 2 pontos.
 			pontuar(-2);
-		}else{	
+		}else{
 			// se nao penalizar o agente em 1.
 			pontuar(-1);
 		}
-		player.direcao = newSentido;    
+		player.direcao = newSentido;
 	}
 }
 
 // Função para alterar a pontuação do agente ao pegar o ouro.
+
 void pegarOuro(){
-    	
-    pontuar(PREMIUM);
+
+	pontuar(PREMIUM);
 }
 
 void atirarFlecha(SENTIDO sentido){
-	
+
 	if(!player.flecha){
-		
+
 		return;
 	}
-	int x = player.x, y = player.y;
-    rotacionar(sentido);	
-	
+	int linha = player.linha;
+	int coluna = player.coluna;
+	rotacionar(sentido);
+
 	switch(sentido){
-		
-		case NORTE:			
-			y--;
+
+		case NORTE:
+			linha--;
 			break;
 		case SUL:
-			y++;
+			linha++;
 			break;
 		case LESTE:
-			x++;
+			coluna++;
 			break;
 		case OESTE:
-			x--;
+			coluna--;
 			break;
 		default:
 			break;
 	}
-	if(verifica_estado(mapa, y, x, WUMPUS)){	
+	if(verifica_estado(mapa, linha, coluna, WUMPUS)){
 
-		retirar_estado(mapa, x, y, WUMPUS);
+		retirar_estado(mapa, linha, coluna, WUMPUS);
 	}
-    pontuar(THROW_ARROW);
+	pontuar(THROW_ARROW);
 }
 
 void agir(ACAO acao, SENTIDO sentido){
-	
-    switch(acao){
-	
+
+	switch(acao){
+
 		case ANDAR:
 			andar(sentido);
-			break;                
+			break;
 		case ATIRAR:
-			break;               
+			break;
 		case PEGAR:
 			pegarOuro();
 			break;
 
-		default :
+		default:
 			/*tratamento de erro*/
 			break;
-		}
+	}
 }
 
 void imprime_mundo_conhecido(){
@@ -126,50 +129,52 @@ void imprime_mundo_conhecido(){
 }
 
 /*Remove suposições a estados que estão adjacentes à posição que o agente está*/
+
 /*TO-DO: Não alterar estados que são visitados*/
 void ag_retirar_estado(ESTADO estado){
-	int x = player.x;
-	int y = player.y;
+	int linha = player.linha;
+	int coluna = player.coluna;
 	/*Estados adjacentes*/
-	if(x > 0 ) //OESTE
-		player.mundo_conhecido[y][x-1] &= ~estado;
-	if(x < TAM_MAPA) //LESTE
-		player.mundo_conhecido[y][x+1] &= ~estado;
-	if(y > 0) //NORTE
-		player.mundo_conhecido[y-1][x] &= ~estado;
-	if(y < TAM_MAPA) //SUL
-		player.mundo_conhecido[y+1][x] &= ~estado;
+	if(coluna > 0) //OESTE
+		player.mundo_conhecido[linha][coluna - 1] &= ~estado;
+	if(coluna < TAM_MAPA) //LESTE
+		player.mundo_conhecido[linha][coluna + 1] &= ~estado;
+	if(linha > 0) //NORTE
+		player.mundo_conhecido[linha - 1][coluna] &= ~estado;
+	if(linha < TAM_MAPA) //SUL
+		player.mundo_conhecido[linha + 1][coluna] &= ~estado;
 }
 
 /*Adiciona suposições a estados que estão adjacentes à posição que o agente está*/
+
 /*TO-DO: Não alterar estados que são visitados*/
 void ag_adicionar_estado(ESTADO estado){
-	int x = player.x;
-	int y = player.y;
+	int linha = player.linha;
+	int coluna = player.coluna;
 	/*Estados adjacentes*/
-	if(x > 0 && !verifica_estado(player.mundo_conhecido,y,x-1,CONHECIDO|VISITADO))//OESTE
-		player.mundo_conhecido[y][x-1] |= estado;
-	if(x < TAM_MAPA && !verifica_estado(player.mundo_conhecido,y,x+1,CONHECIDO|VISITADO))//LESTE
-		player.mundo_conhecido[y][x+1] |= estado;
-	if(y > 0 && !verifica_estado(player.mundo_conhecido,y-1,x,CONHECIDO|VISITADO))//NORTE
-		player.mundo_conhecido[y-1][x] |= estado;
-	if(y < TAM_MAPA && !verifica_estado(player.mundo_conhecido,y+1,x,CONHECIDO|VISITADO))//SUL
-		player.mundo_conhecido[y+1][x] |= estado;
+	if(coluna > 0 && !verifica_estado(player.mundo_conhecido, linha, coluna - 1, CONHECIDO | VISITADO))//OESTE
+		player.mundo_conhecido[linha][coluna - 1] |= estado;
+	if(coluna < TAM_MAPA && !verifica_estado(player.mundo_conhecido, linha, coluna + 1, CONHECIDO | VISITADO))//LESTE
+		player.mundo_conhecido[linha][coluna + 1] |= estado;
+	if(linha > 0 && !verifica_estado(player.mundo_conhecido, linha - 1, coluna, CONHECIDO | VISITADO))//NORTE
+		player.mundo_conhecido[linha - 1][coluna] |= estado;
+	if(linha < TAM_MAPA && !verifica_estado(player.mundo_conhecido, linha + 1, coluna, CONHECIDO | VISITADO))//SUL
+		player.mundo_conhecido[linha + 1][coluna] |= estado;
 }
 
 /*Define as respectivas suposições em relação a posição a que o agente está presente*/
 int marcar_estados_adj(){
 	int estado = 0;
-	int x = player.x, 
-		y = player.y;
+	int linha = player.linha;
+	int coluna = player.coluna;
 
 	//Verifica se na posição possui brisa ou fedor
-	for(estado = 1; estado < 4; estado*=2){
+	for(estado = 1; estado < 4; estado *= 2){
 		//Se não houver retire dos que estão em volta
-		if(!verifica_estado(player.mundo_conhecido,y,x,estado)){
+		if(!verifica_estado(player.mundo_conhecido, linha, coluna, estado)){
 			ag_retirar_estado(estado * 4);
 			ag_adicionar_estado(CONHECIDO);
-		//Se houver verifica se não foi retirado
+			//Se houver verifica se não foi retirado
 		}else{
 			ag_adicionar_estado(estado * 4);
 		}
@@ -179,61 +184,76 @@ int marcar_estados_adj(){
 
 /*Sempre inicia no 0 o nivel da função, logo passe apenas 0*/
 ACAO gera_acao(int nivel){
-	int i,j;
+	int i, j;
 	int livre = 0, wumpus = 0, poco = 0;
-	int pai_x = 1, pai_y = 1;
+	int pai_coluna = 1, pai_linha = 1;
 
-	int x = player.x,
-		y = player.y;
+	int linha = player.linha;
+	int coluna = player.coluna;
 
 	//Pega todos os atributos da posição atual
-	player.mundo_conhecido[player.y][player.x] = 
-			mapa[player.y][player.x];
+	player.mundo_conhecido[player.linha][player.coluna] =
+		mapa[player.linha][player.coluna];
 
-	/* Se a posição do agente não foi visitado, a define
+	/* Se a posição do agente não foi visitada, a define
 	 * como visitado */
-	if(!verifica_estado(player.mundo_conhecido,y,x,VISITADO)){
-		player.mundo_conhecido[y][x] &= ~CONHECIDO;
-		player.mundo_conhecido[y][x] |= VISITADO;
+	if(!verifica_estado(player.mundo_conhecido, linha, coluna, VISITADO)){
+		player.mundo_conhecido[linha][coluna] &= ~CONHECIDO; //Isso aqui não pode existir.
+		player.mundo_conhecido[linha][coluna] |= VISITADO;
 	}
 
 	marcar_estados_adj();
 
-	///Adicionando a matriz que faz a manutenção no estado
-	char matriz_estado[3][TAM_MAPA]= {{0}};
+	//Adicionando a matriz que faz a manutenção no estado
+	char matriz_estado[3][TAM_MAPA] = {
+		{0}};
 
-	///Se o nivel for diferente de 0, indica que ele tem pai
+	//Se o nivel for diferente de 0, indica que ele tem pai
 	if(nivel != 0){
 		//Captura o sentido do jogador
 		switch(player.direcao){
-			case NORTE: pai_y =-1; pai_x = 0;	break;
-			case LESTE: pai_y = 0; pai_x = 1;	break;
-			case SUL:   pai_y = 1; pai_x = 0;	break;
-			case OESTE: pai_y = 0; pai_x =-1;	break;
+			case NORTE:
+				pai_linha = -1;
+				pai_coluna = 0;
+				break;
+			case LESTE:
+				pai_linha = 0;
+				pai_coluna = 1;
+				break;
+			case SUL:
+				pai_linha = 1;
+				pai_coluna = 0;
+				break;
+			case OESTE:
+				pai_linha = 0;
+				pai_coluna = -1;
+				break;
 		}
-		matriz_estado[0][livre++] = (y+pai_y)*10+(x+pai_x);///Captura o pai 
+		matriz_estado[0][livre++] = (linha + pai_linha) * 10 + (coluna + pai_coluna); //Captura o pai 
 	}
 
 	/*Monta a matriz com as possibilidades de movimento*/
 	for(i = -1; i <= 1; i++){
 		for(j = -1; j <= 1; j++){
-			if(i==j || i+j == 0 || (i == pai_y && j == pai_x)) continue;
-			printf("%d %d: %d\n",(y+i), (x+j),(y+i)*10 + (x+j));
-			if(verifica_estado(player.mundo_conhecido,y+i,x+j, WUMPUS)){
-				matriz_estado[1][wumpus++] = (y+i)*10 + (x+j);
-			}else if(verifica_estado(player.mundo_conhecido,y+i,x+j, POCO)){
-				matriz_estado[2][poco++] = (y+i)*10 + (x+j);
-			}else if(verifica_estado(player.mundo_conhecido,y+i,x+j,TODOS_ESTADOS)){
-				matriz_estado[0][livre++] = (y+i)*10 + (x+j);
+			if(i == j || i + j == 0 || (i == pai_linha && j == pai_coluna)) continue;
+			printf("%d %d: %d\n", (linha + i), (coluna + j), (linha + i)*10 + (coluna + j));
+			if(verifica_estado(player.mundo_conhecido, linha + i, coluna + j, WUMPUS)){
+				matriz_estado[1][wumpus++] = (linha + i)*10 + (coluna + j);
+			}else if(verifica_estado(player.mundo_conhecido, linha + i, coluna + j, POCO)){
+				matriz_estado[2][poco++] = (linha + i)*10 + (coluna + j);
+			}else if(verifica_estado(player.mundo_conhecido, linha + i, coluna + j, TODOS_ESTADOS)){
+				matriz_estado[0][livre++] = (linha + i)*10 + (coluna + j);
 			}
 		}
-	}printf("\n");
+	}
+	printf("\n");
 
 	for(i = 0; i < 3; i++){
-		printf("%d: ",i);
+		printf("%d: ", i);
 		for(j = 0; j < 4; j++){
-			printf("%d ",matriz_estado[i][j]);
-		}printf("\n");
+			printf("%d ", matriz_estado[i][j]);
+		}
+		printf("\n");
 	}
 
 	return 0;
--- src/ambiente.c
+++ src/ambiente.c
@@ -5,31 +5,32 @@ void carregar_mapa(char *nome_arquivo, char matriz[TAM_MAPA][TAM_MAPA], int flag
 	FILE *arquivo;
 	//Flag = 1: O mapa é carregado do arquivo. Flag = 0: O mapa foi gerado aleatoriamente e passado por parâmetro.
 	if(flag){
-		arquivo=fopen(nome_arquivo,"r");
+		arquivo = fopen(nome_arquivo, "r");
 		if(arquivo == NULL){
 			printf("Esse aquivo nao existe.");
 			exit(0);
 		}
 	}
-	int posicao_x,posicao_y;
+	int posicao_x, posicao_y;
 	int ouro_iniciado = 0, wumpus_iniciado = 0, quantidade_pocos = 0;
 	char estado;
-	for(posicao_y=0;posicao_y<TAM_MAPA;posicao_y++){
-		for(posicao_x=0;posicao_x<TAM_MAPA;posicao_x++){
+	for(posicao_y = 0; posicao_y < TAM_MAPA; posicao_y++){
+		for(posicao_x = 0; posicao_x < TAM_MAPA; posicao_x++){
 			if(flag){
-				fscanf(arquivo,"%c",&estado);
+				fscanf(arquivo, "%c", &estado);
 			}else{
-				estado=matriz[posicao_y][posicao_x];
+				estado = matriz[posicao_y][posicao_x];
 			}
-			printf("%c",estado);
+			printf("%c", estado);
 			switch(estado){
-				case 'D':{
+				case 'D':
+				{
 					if(!wumpus_iniciado){
-						if(quantidade_pocos<3){
-							mapa[posicao_y][posicao_x]|=WUMPUS+POCO;
-							somar_rastros(FEDOR,posicao_y,posicao_x);
-							somar_rastros(BRISA,posicao_y,posicao_x);
-							wumpus_iniciado=1;
+						if(quantidade_pocos < 3){
+							mapa[posicao_y][posicao_x] |= WUMPUS + POCO;
+							somar_rastros(FEDOR, posicao_y, posicao_x);
+							somar_rastros(BRISA, posicao_y, posicao_x);
+							wumpus_iniciado = 1;
 							quantidade_pocos++;
 						}else{
 							printf("O mapa só pode possuir ate 3 pocos.\n");
@@ -41,21 +42,23 @@ void carregar_mapa(char *nome_arquivo, char matriz[TAM_MAPA][TAM_MAPA], int flag
 					}
 					break;
 				}
-				case 'W':{
+				case 'W':
+				{
 					if(!wumpus_iniciado){
-						mapa[posicao_y][posicao_x]|=WUMPUS;
-						somar_rastros(FEDOR,posicao_y,posicao_x);
-						wumpus_iniciado=1;
+						mapa[posicao_y][posicao_x] |= WUMPUS;
+						somar_rastros(FEDOR, posicao_y, posicao_x);
+						wumpus_iniciado = 1;
 					}else{
 						printf("O mapa só pode possuir um Wumpus.\n");
 						exit(0);
 					}
 					break;
 				}
-				case 'P':{
-					if(quantidade_pocos<3){
-						mapa[posicao_y][posicao_x]|=POCO;
-						somar_rastros(BRISA,posicao_y,posicao_x);
+				case 'P':
+				{
+					if(quantidade_pocos < 3){
+						mapa[posicao_y][posicao_x] |= POCO;
+						somar_rastros(BRISA, posicao_y, posicao_x);
 						quantidade_pocos++;
 					}else{
 						printf("O mapa só pode possuir ate 3 pocos.\n");
@@ -63,17 +66,19 @@ void carregar_mapa(char *nome_arquivo, char matriz[TAM_MAPA][TAM_MAPA], int flag
 					}
 					break;
 				}
-				case 'R':{
+				case 'R':
+				{
 					if(!ouro_iniciado){
-						mapa[posicao_y][posicao_x]|=RELUSENTE;
-						ouro_iniciado=1;
+						mapa[posicao_y][posicao_x] |= RELUSENTE;
+						ouro_iniciado = 1;
 					}else{
 						printf("Erro: O mapa só pode possuir uma barra de ouro.\n");
 						exit(0);
 					}
 					break;
 				}
-				case 'J':{
+				case 'J':
+				{
 					/*if(!jogador_iniciado){
 						if((posicao_y == 0 || posicao_y == TAM_MAPA-1) && (posicao_x == 0 || posicao_x == TAM_MAPA-1)){
 							
@@ -89,71 +94,72 @@ void carregar_mapa(char *nome_arquivo, char matriz[TAM_MAPA][TAM_MAPA], int flag
 					}*/
 					break;
 				}
-				case '-':{
+				case '-':
+				{
 					break;
 				}
-				default:{
-					printf("\nCaracter invalido encontrado no arquivo:'%c'.\n",estado);
+				default:
+				{
+					printf("\nCaracter invalido encontrado no arquivo:'%c'.\n", estado);
 					exit(0);
 				}
 			}
-			if(posicao_x+1<TAM_MAPA){
+			if(posicao_x + 1 < TAM_MAPA){
 				if(flag)
-					fscanf(arquivo," ");
+					fscanf(arquivo, " ");
 				printf(" ");
 			}
 		}
 		if(flag)
-			fscanf(arquivo,"\n");
+			fscanf(arquivo, "\n");
 		printf("\n");
-	}	
+	}
 	if(!ouro_iniciado){
 		printf("\nO mapa nao possui nenhum ouro!\n");
 		exit(0);
-	}	
-	printf("\n\n");	
+	}
+	printf("\n\n");
 	if(flag)
 		fclose(arquivo);
 }
 
-int verifica_estado(char mapa[TAM_MAPA][TAM_MAPA], int x, int y, ESTADO estado){
-	
-	if(y >= 0 && y < 4 && x >=0 && x < 4){
-		return ((mapa[x][y] & (estado)) == estado);
+int verifica_estado(char mapa[TAM_MAPA][TAM_MAPA], int linha, int coluna, ESTADO estado){
+	if(coluna >= 0 && coluna < 4 && linha >= 0 && linha < 4){
+		return((mapa[linha][coluna] & (estado)) == estado);
 	}
 	return 0;
 }
 
 void iniciar_mapa(){
-	int i,j;
-	for(i=0;i<TAM_MAPA;i++){
-		for(j=0;j<TAM_MAPA;j++){
-			mapa[i][j]=0;
+	int i, j;
+	for(i = 0; i < TAM_MAPA; i++){
+		for(j = 0; j < TAM_MAPA; j++){
+			mapa[i][j] = 0;
 		}
 	}
 }
 
 void iniciar_mapa_arquivo(char matriz[TAM_MAPA][TAM_MAPA]){
-	int i,j;
-	for(i=0;i<TAM_MAPA;i++){
-		for(j=0;j<TAM_MAPA;j++){
-			matriz[i][j]='-';
+	int i, j;
+	for(i = 0; i < TAM_MAPA; i++){
+		for(j = 0; j < TAM_MAPA; j++){
+			matriz[i][j] = '-';
 		}
 	}
 }
 
-void somar_rastros(char valor, int posicao_y, int posicao_x){
-	if(posicao_x-1>=0)//Esquerda
-		mapa[posicao_y][posicao_x-1]|=valor;
+void somar_rastros(char valor, int linha, int coluna){
+	if(coluna - 1 >= 0)//Esquerda
+		mapa[linha][coluna - 1] |= valor;
 
-	if(posicao_y-1>=0)//Cima
-		mapa[posicao_y-1][posicao_x]|=valor;
-	
-	if(posicao_x+1<TAM_MAPA)//Direita
-		mapa[posicao_y][posicao_x+1]|=valor;
-	
-	if(posicao_y+1<TAM_MAPA)//Baixo
-		mapa[posicao_y+1][posicao_x]|=valor;
+	if(linha - 1 >= 0)//Cima
+		mapa[linha - 1][coluna] |= valor;
+
+	if(coluna + 1 < TAM_MAPA)//Direita
+		mapa[linha][coluna + 1] |= valor;
+
+	if(linha + 1 < TAM_MAPA)//Baixo
+		mapa[linha + 1][coluna] |= valor;
 }
 
 void gerar_mapa(char *nome_arquivo){
@@ -161,146 +167,146 @@ void gerar_mapa(char *nome_arquivo){
 	iniciar_mapa_arquivo(mapa_arquivo);
 
 	//Escolhendo a posição inicial do jogador.
-	int jogador_pos_x=TAM_MAPA-1,jogador_pos_y=0;
-	mapa_arquivo[jogador_pos_x][jogador_pos_y]='J';
+	int jogador_pos_x = TAM_MAPA - 1, jogador_pos_y = 0;
+	mapa_arquivo[jogador_pos_x][jogador_pos_y] = 'J';
 
 	//Gerando a posicao do ouro;
-	int ouro_x,ouro_y;
+	int ouro_x, ouro_y;
 	do{
-		ouro_x=rand()%4;
-		ouro_y=rand()%4;
-	}while(mapa_arquivo[ouro_x][ouro_y]!='-' && verificar_redor_inicial_jogador(jogador_pos_x,jogador_pos_y,ouro_x,ouro_y));
-	mapa_arquivo[ouro_x][ouro_y]='R';
-	
+		ouro_x = rand() % 4;
+		ouro_y = rand() % 4;
+	}while(mapa_arquivo[ouro_x][ouro_y] != '-' && verificar_redor_inicial_jogador(ouro_x, ouro_y));
+	mapa_arquivo[ouro_x][ouro_y] = 'R';
+
 	//Gerando as posições dos poços. Sempre serão gerados 3 poços.
-	int pocos_validos=0;
+	int pocos_validos = 0;
 	while(!pocos_validos){
 		int posicoes[3][2];
-		int valido=0;
+		int valido = 0;
 		int i;
-		for(i=0;i<3;i++){
+		for(i = 0; i < 3; i++){
 			while(!valido){
-				posicoes[i][0]=rand()%4;
-				posicoes[i][1]=rand()%4;
-				int pos0=posicoes[i][0], pos1=posicoes[i][1];
-				if(mapa_arquivo[pos0][pos1]=='-' && verificar_redor_inicial_jogador(jogador_pos_x,jogador_pos_y,pos0,pos1)){
-					mapa_arquivo[pos0][pos1]='P';
-					valido=1;
+				posicoes[i][0] = rand() % 4;
+				posicoes[i][1] = rand() % 4;
+				int pos0 = posicoes[i][0], pos1 = posicoes[i][1];
+				if(mapa_arquivo[pos0][pos1] == '-' && verificar_redor_inicial_jogador(pos0, pos1)){
+					mapa_arquivo[pos0][pos1] = 'P';
+					valido = 1;
 				}
 			}
-			valido=0;
+			valido = 0;
 		}
-		
-		if(verificar_solucao_possivel_mapa(ouro_x,ouro_y,mapa_arquivo)){
-			pocos_validos=1;
+
+		if(verificar_solucao_possivel_mapa(ouro_x, ouro_y, mapa_arquivo)){
+			pocos_validos = 1;
 		}else{
-			mapa_arquivo[posicoes[0][0]][posicoes[0][1]]='-';
-			mapa_arquivo[posicoes[1][0]][posicoes[1][1]]='-';
-			mapa_arquivo[posicoes[2][0]][posicoes[2][1]]='-';
+			mapa_arquivo[posicoes[0][0]][posicoes[0][1]] = '-';
+			mapa_arquivo[posicoes[1][0]][posicoes[1][1]] = '-';
+			mapa_arquivo[posicoes[2][0]][posicoes[2][1]] = '-';
 		}
-	}
-	
-	//Gerando posição do Wumpus.
-	int wumpus_x,wumpus_y;
-	do{
-		wumpus_x=rand()%4;
-		wumpus_y=rand()%4;
-	}while(mapa_arquivo[wumpus_x][wumpus_y]!='-' && mapa_arquivo[wumpus_x][wumpus_y]!='P');
-	if(mapa_arquivo[wumpus_x][wumpus_y]=='P'){
-		mapa_arquivo[wumpus_x][wumpus_y]='D';
-	}else{
-		mapa_arquivo[wumpus_x][wumpus_y]='W';
 	}
 
-	criar_arquivo(nome_arquivo,mapa_arquivo);
-	carregar_mapa(nome_arquivo,mapa_arquivo,0);
+	//Gerando posição do Wumpus.
+	int wumpus_x, wumpus_y;
+	do{
+		wumpus_x = rand() % 4;
+		wumpus_y = rand() % 4;
+	}while(mapa_arquivo[wumpus_x][wumpus_y] != '-' && mapa_arquivo[wumpus_x][wumpus_y] != 'P');
+	if(mapa_arquivo[wumpus_x][wumpus_y] == 'P'){
+		mapa_arquivo[wumpus_x][wumpus_y] = 'D';
+	}else{
+		mapa_arquivo[wumpus_x][wumpus_y] = 'W';
+	}
+
+	criar_arquivo(nome_arquivo, mapa_arquivo);
+	carregar_mapa(nome_arquivo, mapa_arquivo, 0);
 }
 
 int verificar_mapa_valido(){
-	
+
 	return 1;
 }
 
-int verificar_solucao_possivel_mapa(int posicao_x, int posicao_y, char matriz[TAM_MAPA][TAM_MAPA]){
+int verificar_solucao_possivel_mapa(int linha, int coluna, char matriz[TAM_MAPA][TAM_MAPA]){
 	//Verifica o redor do ouro para descobrir se é possível que o jogador possa pegá-lo.
-	if(posicao_x==0 && (posicao_y>0 && posicao_y<TAM_MAPA-1)){//Linha 0
-		if(matriz[posicao_x][posicao_y-1] =='P' && matriz[posicao_x+1][posicao_y]=='P' && matriz[posicao_x][posicao_y+1] == 'P')
+	if(linha == 0 && (coluna > 0 && coluna < TAM_MAPA - 1)){//Linha 0
+		if(matriz[linha][coluna - 1] == 'P' && matriz[linha + 1][coluna] == 'P' && matriz[linha][coluna + 1] == 'P')
 			return 0;
 		return 1;
 	}
-	
-	if(posicao_x==TAM_MAPA-1 && (posicao_y>0 && posicao_y<TAM_MAPA-1)){//Linha TAM_MAPA-1
-		if(matriz[posicao_x][posicao_y-1] =='P' && matriz[posicao_x-1][posicao_y]=='P' && matriz[posicao_x][posicao_y+1] == 'P')
+
+	if(linha == TAM_MAPA - 1 && (coluna > 0 && coluna < TAM_MAPA - 1)){//Linha TAM_MAPA-1
+		if(matriz[linha][coluna - 1] == 'P' && matriz[linha - 1][coluna] == 'P' && matriz[linha][coluna + 1] == 'P')
 			return 0;
 		return 1;
 	}
-	
-	if((posicao_x>0 && posicao_x<TAM_MAPA-1) && posicao_y==0){//Coluna 0
-		if(matriz[posicao_x-1][posicao_y] =='P' && matriz[posicao_x][posicao_y+1]=='P' && matriz[posicao_x+1][posicao_y] == 'P')
+
+	if((linha > 0 && linha < TAM_MAPA - 1) && coluna == 0){//Coluna 0
+		if(matriz[linha - 1][coluna] == 'P' && matriz[linha][coluna + 1] == 'P' && matriz[linha + 1][coluna] == 'P')
 			return 0;
 		return 1;
 	}
-	
-	if((posicao_x>0 && posicao_x<TAM_MAPA-1) && posicao_y==TAM_MAPA-1){//Coluna TAM_MAPA-1
-		if(matriz[posicao_x-1][posicao_y] =='P' && matriz[posicao_x][posicao_y-1]=='P' && matriz[posicao_x+1][posicao_y] == 'P')
+
+	if((linha > 0 && linha < TAM_MAPA - 1) && coluna == TAM_MAPA - 1){//Coluna TAM_MAPA-1
+		if(matriz[linha - 1][coluna] == 'P' && matriz[linha][coluna - 1] == 'P' && matriz[linha + 1][coluna] == 'P')
 			return 0;
 		return 1;
 	}
-	
-	if(posicao_x == 0 && posicao_y ==0){//Posicao [0,0]
-		if(matriz[posicao_x+1][posicao_y]=='P' && matriz[posicao_x][posicao_y+1]=='P')
+
+	if(linha == 0 && coluna == 0){//Posicao [0,0]
+		if(matriz[linha + 1][coluna] == 'P' && matriz[linha][coluna + 1] == 'P')
 			return 0;
-		if(matriz[posicao_x+1][posicao_y]=='P' && matriz[posicao_x+1][posicao_y+1]=='P' && matriz[posicao_x][posicao_y+2]=='P')
+		if(matriz[linha + 1][coluna] == 'P' && matriz[linha + 1][coluna + 1] == 'P' && matriz[linha][coluna + 2] == 'P')
 			return 0;
-		if(matriz[posicao_x][posicao_y+1]=='P' && matriz[posicao_x+1][posicao_y+1]=='P' && matriz[posicao_x+2][posicao_y]=='P')
-			return 0;
-		return 1;
-	}
-	
-	if(posicao_x == 0 && posicao_y ==TAM_MAPA-1){//Posicao [0,TAM_MAPA-1]
-		if(matriz[posicao_x+1][posicao_y]=='P' && matriz[posicao_x][posicao_y-1]=='P')
-			return 0;
-		if(matriz[posicao_x+1][posicao_y]=='P' && matriz[posicao_x+1][posicao_y-1]=='P' && matriz[posicao_x][posicao_y-2]=='P')
-			return 0;
-		if(matriz[posicao_x][posicao_y-1]=='P' && matriz[posicao_x+1][posicao_y-1]=='P' && matriz[posicao_x+2][posicao_y]=='P')
+		if(matriz[linha][coluna + 1] == 'P' && matriz[linha + 1][coluna + 1] == 'P' && matriz[linha + 2][coluna] == 'P')
 			return 0;
 		return 1;
 	}
-	
-	if(posicao_x == TAM_MAPA-1 && posicao_y ==TAM_MAPA-1){//Posicao
-		if(matriz[posicao_x-1][posicao_y]=='P' && matriz[posicao_x][posicao_y-1]=='P')
+
+	if(linha == 0 && coluna == TAM_MAPA - 1){//Posicao [0,TAM_MAPA-1]
+		if(matriz[linha + 1][coluna] == 'P' && matriz[linha][coluna - 1] == 'P')
 			return 0;
-		if(matriz[posicao_x-1][posicao_y]=='P' && matriz[posicao_x-1][posicao_y-1]=='P' && matriz[posicao_x][posicao_y-2]=='P')
+		if(matriz[linha + 1][coluna] == 'P' && matriz[linha + 1][coluna - 1] == 'P' && matriz[linha][coluna - 2] == 'P')
 			return 0;
-		if(matriz[posicao_x][posicao_y-1]=='P' && matriz[posicao_x-1][posicao_y-1]=='P' && matriz[posicao_x-2][posicao_y]=='P')
+		if(matriz[linha][coluna - 1] == 'P' && matriz[linha + 1][coluna - 1] == 'P' && matriz[linha + 2][coluna] == 'P')
+			return 0;
+		return 1;
+	}
+
+	if(linha == TAM_MAPA - 1 && coluna == TAM_MAPA - 1){//Posicao
+		if(matriz[linha - 1][coluna] == 'P' && matriz[linha][coluna - 1] == 'P')
+			return 0;
+		if(matriz[linha - 1][coluna] == 'P' && matriz[linha - 1][coluna - 1] == 'P' && matriz[linha][coluna - 2] == 'P')
+			return 0;
+		if(matriz[linha][coluna - 1] == 'P' && matriz[linha - 1][coluna - 1] == 'P' && matriz[linha - 2][coluna] == 'P')
 			return 0;
 		return 1;
 	}
 	return 1;
 }
 
-int verificar_redor_inicial_jogador(int jogador_x, int jogador_y, int posicao_x, int posicao_y){
-	if((posicao_x==TAM_MAPA-2 && posicao_y==0) || (posicao_x==TAM_MAPA-1 && posicao_y==1) || (posicao_x==TAM_MAPA-2 && posicao_y==1))
-		return 0;	
+int verificar_redor_inicial_jogador(int linha, int coluna){
+	if((linha == TAM_MAPA - 2 && coluna == 0) || (linha == TAM_MAPA - 1 && coluna == 1) || (linha == TAM_MAPA - 2 && coluna == 1))
+		return 0;
 	return 1;
 }
 
 void criar_arquivo(char *nome_arquivo, char matriz[TAM_MAPA][TAM_MAPA]){
-	FILE *arquivo = fopen(nome_arquivo,"w");
-	int i,j;
-	for(i=0;i<TAM_MAPA;i++){
-		for(j=0;j<TAM_MAPA;j++){
-			fprintf(arquivo,"%c",matriz[i][j]);
-			if(j<TAM_MAPA-1){
-				fprintf(arquivo," ");
+	FILE *arquivo = fopen(nome_arquivo, "w");
+	int i, j;
+	for(i = 0; i < TAM_MAPA; i++){
+		for(j = 0; j < TAM_MAPA; j++){
+			fprintf(arquivo, "%c", matriz[i][j]);
+			if(j < TAM_MAPA - 1){
+				fprintf(arquivo, " ");
 			}
 		}
-		fprintf(arquivo,"\n");
+		fprintf(arquivo, "\n");
 	}
 	fclose(arquivo);
 }
 
-void __imprime_mapa_linha(char hor, char vert, int x){	
+void __imprime_mapa_linha(char hor, char vert, int x){
 	int i;
 	for(i = 0; i < (TAM_MAPA * x) + TAM_MAPA + 1; i++){
 		printf("%c", (i % TAM_MAPA) ? hor : vert);
@@ -315,15 +321,18 @@ void imprime_mapa(char mapa[TAM_MAPA][TAM_MAPA]){
 	int bit;
 
 	char simbolos[7][10] = {" ", "\033[1;34mB", "\033[1;35mF", "\033[0;34mP", "\033[1;31mW", "\033[01;33mR", "\033[1;32mJ"};
+
+	char c = 0;
 	
 	__imprime_mapa_linha(hor, vert, x);
-	for(i=0; i<TAM_MAPA; i++){
-		for(l=0; l<y; l++){
+	for(i = 0; i < TAM_MAPA; i++){
+		for(l = 0; l < y; l++){
 			printf("%c", vert);
-			for(j=0; j<TAM_MAPA; j++){
-				for(k=0; k<x; k++){
-					bit = k + (l*x);
-					printf("%s\033[0m", simbolos[ (bit+1) * verifica_estado(mapa, i, j, 1 << bit) ]);
+			for(j = 0; j < TAM_MAPA; j++){
+				c = mapa[i][j];
+				for(k = 0; k < x; k++){
+					bit = k + (l * x);
+					printf("%s\033[0m", simbolos[ (bit + 1) * verifica_estado(mapa, i, j, 1 << bit) ]);
 				}
 				printf("%c", vert);
 			}
@@ -334,26 +343,24 @@ void imprime_mapa(char mapa[TAM_MAPA][TAM_MAPA]){
 	printf("\n");
 }
 
-void adicionar_estado(char mapa[TAM_MAPA][TAM_MAPA], 
-			int x, int y, ESTADO estado){
-	if(x > 0 && !verifica_estado(mapa,y,x-1,CONHECIDO))
-		mapa[y][x-1] |= estado;
-	if(x < TAM_MAPA && !verifica_estado(mapa,y,x+1,CONHECIDO))
-		mapa[y][x+1] |= estado;
-	if(y > 0 && !verifica_estado(mapa,y-1,x,CONHECIDO))
-		mapa[y-1][x] |= estado;
-	if(y < TAM_MAPA && !verifica_estado(mapa,y+1,x,CONHECIDO))
-		mapa[y+1][x] |= estado;
+void adicionar_estado(char mapa[TAM_MAPA][TAM_MAPA], int linha, int coluna, ESTADO estado){
+	if(coluna > 0 && !verifica_estado(mapa, linha, coluna - 1, CONHECIDO))
+		mapa[linha][coluna - 1] |= estado;
+	if(coluna < TAM_MAPA && !verifica_estado(mapa, linha, coluna + 1, CONHECIDO))
+		mapa[linha][coluna + 1] |= estado;
+	if(linha > 0 && !verifica_estado(mapa, linha - 1, coluna, CONHECIDO))
+		mapa[linha - 1][coluna] |= estado;
+	if(linha < TAM_MAPA && !verifica_estado(mapa, linha + 1, coluna, CONHECIDO))
+		mapa[linha + 1][coluna] |= estado;
 }
 
-void retirar_estado(char mapa[TAM_MAPA][TAM_MAPA], 
-			int x, int y, ESTADO estado){
-	if(x > 0)
-		mapa[y][x-1] &= ~estado;
-	if(x < TAM_MAPA)
-		mapa[y][x+1] &= ~estado;
-	if(y > 0)
-		mapa[y-1][x] &= ~estado;
-	if(y < TAM_MAPA)
-		mapa[y+1][x] &= ~estado;
+void retirar_estado(char mapa[TAM_MAPA][TAM_MAPA], int linha, int coluna, ESTADO estado){
+	if(coluna > 0)
+		mapa[linha][coluna - 1] &= ~estado;
+	if(coluna < TAM_MAPA)
+		mapa[linha][coluna + 1] &= ~estado;
+	if(linha > 0)
+		mapa[linha - 1][coluna] &= ~estado;
+	if(linha < TAM_MAPA)
+		mapa[linha + 1][coluna] &= ~estado;
 }
